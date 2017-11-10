1577fb2561c0632179a200d7ff525d1ca6140f29
Katherine Hyland <katherinelizabethyland@gmail.com>
2017-11-09 21:38:54 -0500

    added display controller

--- displaycontroller.js
+++ displaycontroller.js
@@ -1,12 +1,12 @@
 var app = angular.module('MadLibs');
-app.controller('displayController', function ($scope, MadLibsFactory){
+app.controller('displayController', function($scope, MadLibsFactory){
   $scope.nounName=MadLibsFactory.nounName,
   $scope.verbActivity=MadLibsFactory.verbActivity,
   $scope.nounPlace=MadLibsFactory.nounPlace,
   $scope.nounName= MadLibsFactory.nounName,
   $scope.nounThing=MadLibsFactory.nounThing,
   $scope.nounName=MadLibsFactory.nounName,
-  $scope.nounNewname= MadLibsFactory.NewName
+  $scope.nounNewName=MadLibsFactory.nounNewName
 });
 // app.controller('displayController', function($scope, MadLibsFactory) {
 //   $scope.data = MadLibsFactory.getData();
