72793e7e6153eb5ae8a78d742699f0f363e3c8de
Reema Husain <reemzh@gmail.com>
2017-11-09 19:38:00 -0500

    inputcontroller changes

--- inputController.js
+++ inputController.js
@@ -1,10 +1,22 @@
+// var app = angular.module('MadLibs');
+// app.controller('inputController', function($scope, MadLibsFactory) {
+//   $scope.getInfo = function(){
+//   MadLibsFactory.nounName= $scope.nounName;
+//   MadLibsFactory.nounName= $scope.verbActivity;
+//   MadLibsFactory.nounName= $scope.nounPlace;
+//   MadLibsFactory.nounName= $scope.nounThing;
+//   MadLibsFactory.nounName= $scope.nounNewName;
+// }
+// });
+
+
 var app = angular.module('MadLibs');
 app.controller('inputController', function($scope, MadLibsFactory) {
-  $scope.getInfo = function(){
-  MadLibsFactory.nounName= $scope.nounName;
-  MadLibsFactory.nounName= $scope.verbActivity;
-  MadLibsFactory.nounName= $scope.nounPlace;
-  MadLibsFactory.nounName= $scope.nounThing;
-  MadLibsFactory.nounName= $scope.nounNewName;
-}
+  $scope.save= function(){
+  MadLibsFactory.nounName= $scope.nounName,
+  MadLibsFactory.verbActivity= $scope.verbActivity,
+  MadLibsFactory.nounPlace= $scope.nounPlace,
+  MadLibsFactory.nounThing= $scope.nounThing,
+  MadLibsFactory.nounNewName= $scope.nounNewName
+  }
 });
