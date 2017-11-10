8df1b2db5556a13fd34cb6f0b68d084b3e3506d8
Katherine Hyland <katherinelizabethyland@gmail.com>
2017-11-09 19:21:20 -0500

    added factory

--- madLibsFactory.js
+++ madLibsFactory.js
@@ -1,24 +1,23 @@
 var app = angular.module('MadLibs');
 app.factory ('MadLibsFactory', function (){
-var newLibs= [];
 return{
-  getLibs
-}
 
-function getLibs () {
-  if (newLibs.length) {
-    return Promise.resolve(newLibs);
-  }
-var url='/index.html';
-return $http.get(url).then(function(response){
-  newLibs= response.getInfo;
-  return newLibs;
-}).catch(function (err){
-  console.log(err);
-});
 };
-
 });
+// function getLibs () {
+//   if (newLibs.length) {
+//     return Promise.resolve(newLibs);
+//   }
+// var url='';
+// return $http.get(url).then(function(response){
+//   newLibs= response.getInfo;
+//   return newLibs;
+// }).catch(function (err){
+//   console.log(err);
+// });
+// };
+
+
 // $http.get('/Users/davidpotter/Desktop/MadLibsAngLab/input.html').then(function (response){
   // data = response;
   // });
